3847fa086a41c1343b948cb11ffe812bf7b218a2
palanceli <palanceli@163.com>
2018-06-28 22:55:46 +0800

    commit changes

--- setupenv.sh
+++ setupenv.sh
@@ -1,14 +1,35 @@
-# git submodule update --init --recursive
-git submodule add https://github.com/google/googletest.git modules/googletest
-cd modules/googletest
-git checkout release-1.8.0
+# # git submodule update --init --recursive
+# git submodule add https://github.com/google/googletest.git modules/googletest
+# cd modules/googletest
+# git checkout release-1.8.0
 
-cd ../..
-git submodule add https://github.com/google/glog.git modules/glog
-cd modules/glog
-git checkout v0.3.5
+# cd ../..
+# git submodule add https://github.com/google/glog.git modules/glog
+# cd modules/glog
+# git checkout v0.3.5
 
-cd ../..
-git submodule add https://github.com/gflags/gflags.git modules/gflags
-cd modules/gflags
-git checkout v2.2.1
+# cd ../..
+# git submodule add https://github.com/gflags/gflags.git modules/gflags
+# cd modules/gflags
+# git checkout v2.2.1
+
+# cd modules/googletest
+# mkdir _build
+# cd _build
+# cmake ..
+# make 
+# make install
+
+# cd modules/gflags
+# mkdir _build
+# cd _build
+# cmake ..
+# make
+# make install
+
+# cd modules/glog
+# mkdir _build
+# cd _build
+# cmake ..
+# make
+# make install
