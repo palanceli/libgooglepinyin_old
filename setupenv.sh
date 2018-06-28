b584b125b54a22d1ab2e370a1486c0310bffe822
palanceli <palanceli@163.com>
2018-06-28 20:39:37 +0800

    commit changes

--- setupenv.sh
+++ setupenv.sh
@@ -1,1 +1,14 @@
-git submodule update --init --recursive
+# git submodule update --init --recursive
+git submodule add https://github.com/google/googletest.git modules/googletest
+cd modules/googletest
+git checkout release-1.8.0
+
+cd ../..
+git submodule add https://github.com/google/glog.git modules/glog
+cd modules/glog
+git checkout v0.3.5
+
+cd ../..
+git submodule add https://github.com/gflags/gflags.git modules/gflags
+cd modules/gflags
+git checkout v2.2.1
