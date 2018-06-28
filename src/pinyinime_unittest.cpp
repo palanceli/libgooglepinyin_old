af9f245cc6d1626c8d85c48e424a39d6ae520f35
palance <palanceli@163.com>
2018-06-28 23:37:17 +0800

    commit changes

--- src/pinyinime_unittest.cpp
+++ src/pinyinime_unittest.cpp
@@ -37,71 +37,71 @@
 using namespace ime_pinyin;
 
 namespace googlepinyin_test{
-    class PinyinImeTest : public testing::Test{
-    protected:
-        static void SetUpTestCase(){
-        }
-        static void TearDownTestCase(){
-            
-        }
-    };
+  class PinyinImeTest : public testing::Test{
+  protected:
+    const char* mSzRawDictPath = "../../data/rawdict_utf16_65105_freq.txt";
+    const char* mSzValidUtfPath = "../../data/valid_utf16.txt";
+    const char* mSzDatFilePath = "../data/dict_pinyin.dat";
     
-    TEST_F(PinyinImeTest, TCGetcwd){
-        char* szDir;
-        if((szDir = (char*)getcwd(NULL, 0)) == NULL){
-            printf("Failed to get cwd\n");
-            return;
-        }
-        LOG(INFO)<<"cwd: "<< szDir;
+  protected:
+    static void SetUpTestCase(){
     }
-    
-    TEST_F(PinyinImeTest, TCBuildDict){
-        setlocale(LC_ALL, "");
-        DictTrie* dict_trie = new DictTrie();
-        bool success;
-        
-        success = dict_trie->build_dict("../../data/rawdict_utf16_65105_freq.txt",
-                                            "../../data/valid_utf16.txt");
-        ASSERT_EQ(success, true) << "Failed to build dict!";
-        
-        success = dict_trie->save_dict("../data/dict_pinyin.dat");
-        ASSERT_EQ(success, true) << "Failed to save dict!";
+    static void TearDownTestCase(){
+      
     }
+  };
+  
+  TEST_F(PinyinImeTest, TCGetcwd){
+    char* szDir;
+    if((szDir = (char*)getcwd(NULL, 0)) == NULL){
+      printf("Failed to get cwd\n");
+      return;
+    }
+    LOG(INFO)<<"cwd: "<< szDir;
+  }
+  
+  TEST_F(PinyinImeTest, TCBuildDict){
+    setlocale(LC_ALL, "");
+    DictTrie* dict_trie = new DictTrie();
+    bool success;
     
-    TEST_F(PinyinImeTest, TC2){
-        setlocale(LC_ALL, "");
-        char const *szSysDict = "../data/dict_pinyin.dat";
-        char const *szUserDict = "";
+    success = dict_trie->build_dict(mSzRawDictPath, mSzValidUtfPath);
+    ASSERT_EQ(success, true) << "Failed to build dict!";
+    
+    success = dict_trie->save_dict(mSzDatFilePath);
+    ASSERT_EQ(success, true) << "Failed to save dict!";
+  }
+  
+  TEST_F(PinyinImeTest, TC2){
+    setlocale(LC_ALL, "");
+    char const *szUserDict = "";
+    
+    bool ret = im_open_decoder(mSzDatFilePath, szUserDict);  // 加载
+    ASSERT_EQ(ret, true) << "Failed to load dict!";
+    
+    im_set_max_lens(32, 16);
+    char szLine[256] = "a";
+    
+    im_reset_search();
+    size_t nr = im_search(szLine, strlen(szLine)); // 查询
+    size_t size = 0;
+    LOG(INFO)<<"输入串："<<im_get_sps_str(&size)<<"\n候选串：";  // 返回拼音串
+    char16 str[64] = { 0 };
+    for (auto i = 0; i < nr; i++)
+    {
+      im_get_candidate(i, str, 32);         // 获取查询候选
+      if(i > 5)
+        break;
 
-        bool ret = im_open_decoder(szSysDict, szUserDict);  // 加载
-        
-        im_set_max_lens(32, 16);
-        char szLine[256] = "a";
-        
-        im_reset_search();
-        size_t nr = im_search(szLine, strlen(szLine)); // 查询
-        size_t size = 0;
-        printf("输入串：%s\n候选串：", im_get_sps_str(&size));  // 返回拼音串
-        char16 str[64] = { 0 };
-        for (auto i = 0; i < nr; i++)
-        {
-            im_get_candidate(i, str, 32);         // 获取查询候选
-            if(i > 5)
-                break;
-#ifdef WIN32
-            const wchar_t* szCand = (const wchar_t*)str;
-            wprintf(L"%s\n", szCand);
-#else
-            std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
-            std::string strCand = convert.to_bytes((char16_t*)str);
-            std::cout << i << "." << strCand << " ";
-#endif
-        }
-        if(nr>5)
-            printf("...");
-        printf(" 共%lu个候选\n", nr);
-    
-        im_close_decoder();                 // 关闭
-    
+      std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+      std::string strCand = convert.to_bytes((char16_t*)str);
+      LOG(INFO)<< i << "." << strCand << "";
     }
+    if(nr>5)
+      LOG(INFO)<<"...";
+    LOG(INFO)<<" 共"<< nr << "个候选\n";
+    
+    im_close_decoder();                 // 关闭
+    
+  }
 }
